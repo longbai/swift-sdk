fc5d75e53cc6f489361bfe46bbc009f4a40049a2
longbai <slongbai@gmail.com>
2014-10-21 21:47:15 +0800

    init

---
+++ swfit-sdkTests/swfit_sdkTests.swift
@@ -0,0 +1,36 @@
+//
+//  swfit_sdkTests.swift
+//  swfit-sdkTests
+//
+//  Created by bailong on 14/10/21.
+//  Copyright (c) 2014年 Qiniu Cloud Storage. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class swfit_sdkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

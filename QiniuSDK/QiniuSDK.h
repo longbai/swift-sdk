f591b44cc237c1ef0d4817210ec57e48c3496f09
longbai <slongbai@gmail.com>
2015-11-07 13:33:56 +0800

    init

---
+++ QiniuSDK/QiniuSDK.h
@@ -0,0 +1,13 @@
+//
+//  QiniuSDK.h
+//  QiniuSDK
+//
+//  Created by bailong on 15/11/6.
+//  Copyright © 2015年 Qiniu Cloud Storage. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QiniuSDK : NSObject
+
+@end
---
+++ QiniuSDK/QiniuSDK.m
@@ -0,0 +1,13 @@
+//
+//  QiniuSDK.m
+//  QiniuSDK
+//
+//  Created by bailong on 15/11/6.
+//  Copyright © 2015年 Qiniu Cloud Storage. All rights reserved.
+//
+
+#import "QiniuSDK.h"
+
+@implementation QiniuSDK
+
+@end

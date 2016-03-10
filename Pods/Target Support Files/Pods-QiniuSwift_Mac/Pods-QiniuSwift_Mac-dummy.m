48b4dcfc39c3c3248d5b9432bc47bc4998447123
longbai <slongbai@gmail.com>
2016-03-11 00:58:04 +0800

    remove ignore

--- Pods/Target Support Files/Pods-QiniuSwift_Mac/Pods-QiniuSwift_Mac-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_QiniuSwift_Mac : NSObject
-@end
-@implementation PodsDummy_Pods_QiniuSwift_Mac
-@end
--- Pods/Target Support Files/Pods-QiniuSwift_Mac/Pods-QiniuSwift_Mac-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-
-FOUNDATION_EXPORT double Pods_QiniuSwift_MacVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_QiniuSwift_MacVersionString[];
-
